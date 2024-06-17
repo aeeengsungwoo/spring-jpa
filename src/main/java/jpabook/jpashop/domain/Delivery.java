638446dfd11746befa3c78ad38c065ea360d80af
ansungwoo <asw4ever@naver.com>
2024-06-17 15:43:17 +0900

    docs: 도메인 설계

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    // Enum Type은 기본적으로 ORDINAL인데, 이건 1 2 3 같은 숫자로 디비에 값이 들어간다. 중간에 status를 추가하면 값이 밀려서 망해버림
+    // 꼭 String으로 설정하자.
+    private DeliveryStatus status; //ENUM [READY(준비), COMP(배송)]
+}
---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "item_id")
+    private Item item; //주문 상품
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order; //주문
+
+    private int orderPrice;
+    private int count; //주문 수량
+}
