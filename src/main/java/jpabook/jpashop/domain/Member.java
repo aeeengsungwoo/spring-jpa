f00f8acad0c1334165e1671134c758d0db11d01c
ansungwoo <asw4ever@naver.com>
2024-06-23 19:35:39 +0900

    feat: 엔티티 설계

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -19,6 +19,11 @@ public class Member {
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
+    /*
+     여기서 orders 의 초기화를 하는데, 이 방법 말고 다른 방법으로 초기화를 하게되면 Null pointer 문제가 발생할 수 있다.
+     따라서 위와 같은 방법으로 초기화를 하고(이 방법은 널 포인터 문제 안생김) 다른 클래스에서 orders를 변경하지 않도록 해야한다.
+     이렇게 객체를 샐성한다는 것은, 이 객체를 DB에 저장한다는 의미인데(영속화 시키겠다는), 다른데서 바꿔버리면 디비 값이랑 달라지는 문제가 생긴다.
+    */
 
 
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,10 +4,13 @@ import jakarta.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.util.Lazy;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import static jakarta.persistence.FetchType.*;
 
 @Entity
 @Table(name = "orders")
@@ -17,13 +20,24 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
-    @OneToMany(mappedBy = "order")
-    private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+    /*
+     CascadeType.ALL 설정을 안해주면
+     persist(orderItemA)
+     persist(orderItemB)
+     persist(orderItemC)
+     persist(order)
+     이렇게 아이템까지 다 save해줘야 한다. 하지만 CascadeType.ALL 설정으로
+     persist(order) 이 한줄로 이전과 같은 결과를 만들 수 있다. Cascade가 persist를 전파하기 때문에 가능한 일이다.
+    */
+
+
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -33,4 +47,17 @@ public class Order {
     private OrderStatus status; //주문의 상태 [ORDER, CANCEL]
 
 
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
