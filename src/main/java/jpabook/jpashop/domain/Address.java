a01f5be656d7b9824b3ea433b95583434f74e4d4
ansungwoo <asw4ever@naver.com>
2024-06-23 18:46:43 +0900

    fix: address 객체 초기화로 변경 불가능하도록 제한

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -9,4 +9,15 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    //내부 값들을 변경하지 못하도록 생성자에서 값을 모두 초기화 해 변경 불가능한 클래스를 만들어준것이다.
+    protected Address(){
+
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
